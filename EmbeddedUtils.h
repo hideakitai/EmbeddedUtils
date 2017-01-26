3e9f6b40f186263cdb2325a97925830b4e402665
Hideaki Tai <hideaki.tai@gmail.com>
2017-01-26 13:59:14 +0900

    first commit

---
+++ EmbeddedUtils.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "lib/Debug/Debug.h"
+#include "lib/Calculus/Calculus.h"
+#include "lib/Convert/Convert.h"
+#include "lib/Filters/Filters.h"
+#include "lib/MatrixFunc/MatrixFunc.h"
+#include "lib/Vec/Vec.h"
