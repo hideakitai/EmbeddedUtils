13f07a6244b0713d53a3e5fa50dd02e89bc30162
Hideaki Tai <hideaki.tai@gmail.com>
2018-02-25 13:24:25 +0900

    eigen first

--- lib/Calculus.h
+++ lib/Calculus.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <Arduino.h>
+#include "Eigen/Eigen"
 
 namespace Calculus
 {
@@ -7,23 +8,56 @@ namespace Calculus
     class Differential
     {
     public:
+
+        Differential() { reset(); }
         Differential(T gain) : gain_(gain) { reset(); }
 
         inline T get(const T& integral, float dt)
         {
             T newVal;
-            newVal  = gain_ * integral - buffer_;
+            newVal  = integral * gain_ - buffer_;
             buffer_ += gain_ * newVal * dt;
             return newVal;
         }
 
-        inline void reset() { buffer_ = T::zero(); }
+        inline void reset() { buffer_.setZero(); }
         inline void setGain(const T& gain) { gain_ = gain; }
 
     private:
         T gain_;
         T buffer_;
     };
+
+//    template <> inline T Differential<float>::get(const T& integral, float dt)
+//    {
+//        T newVal;
+//        newVal  = integral * gain_ - buffer_;
+//        buffer_ += gain_ * newVal * dt;
+//        return newVal;
+//    }
+//
+//    template <> inline T Differential<double>::get(const T& integral, float dt)
+//    {
+//        T newVal;
+//        newVal  = integral * gain_ - buffer_;
+//        buffer_ += gain_ * newVal * dt;
+//        return newVal;
+//    }
+//
+//    template <> inline T Differential<Vec3f>::get(const T& integral, float dt)
+//    {
+//        T newVal;
+//        newVal  = integral * gain_ - buffer_;
+//        buffer_ += gain_ * newVal * dt;
+//        return newVal;
+//    }
+
+    template <> inline Eigen::Vector3f Differential<Eigen::Vector3f>::get(const Eigen::Vector3f& integral, float dt)
+    {
+        Eigen::Vector3f newVal = integral.array() * gain_.array() - buffer_.array();
+        buffer_ = buffer_.array() + gain_.array() * newVal.array() * dt;
+        return newVal;
+    }
 
     template <> inline void Differential<float>::reset() { buffer_ = 0.0; }
     template <> inline void Differential<double>::reset() { buffer_ = 0.0; }
@@ -32,6 +66,7 @@ namespace Calculus
     class Integral
     {
     public:
+
         Integral() { reset(); }
 
         inline const T& get(const T& differential, float dt)
@@ -40,7 +75,7 @@ namespace Calculus
             return buffer_;
         }
 
-        inline void reset() { buffer_ = T::zero(); }
+        inline void reset() { buffer_.setZero(); }
 
 private:
         T buffer_;
--- lib/Filters.h
+++ lib/Filters.h
@@ -7,7 +7,8 @@ namespace Filter
     class LPF
     {
     public:
-        LPF(float gain) : gain_(gain), buffer_(0.0) {}
+        LPF() { reset(); }
+        LPF(T gain) : gain_(gain), buffer_() {}
 
         inline T get(const T& curr_val, float dt)
         {
@@ -18,15 +19,23 @@ namespace Filter
         }
 
         inline void reset() { buffer_ = T::zero(); }
-        inline void setGain(float gain) { gain_ = gain; }
+        inline void setGain(T gain) { gain_ = gain; }
 
     private:
-        double gain_;
+        T gain_;
         T buffer_;
     };
 
+    template <> inline Eigen::Vector3f LPF<Eigen::Vector3f>::get(const Eigen::Vector3f& curr_val, float dt)
+    {
+        Eigen::Vector3f new_val = curr_val.array() + (new_val.array() - buffer_.array()) * gain_.array() * (double)dt;
+        buffer_ = new_val.matrix();
+        return new_val;
+    }
+
     template <> inline void LPF<float>::reset() { buffer_ = 0.0; }
     template <> inline void LPF<double>::reset() { buffer_ = 0.0; }
+    template <> inline void LPF<Eigen::Vector3f>::reset() { buffer_ = Eigen::Vector3f::Zero(); }
 
     template <typename T>
     class HPF
