954dd821b006b5ce821c430cb784fb8412b70c7f
Hideaki Tai <hideaki.tai@gmail.com>
2018-12-19 15:03:09 +0900

    fix avr/Gamma.h for new avr/RingQueue

--- lib/avr/Gamma.h
+++ lib/avr/Gamma.h
@@ -2,12 +2,12 @@
 #ifndef EMBEDDEDUTILS_GAMMA_H
 #define EMBEDDEDUTILS_GAMMA_H
 
-template <typename TYPE, size_t SIZE>
+template <typename TYPE, std::size_t SIZE>
 class GammaTable
 {
 public:
 
-    GammaTable(const float gamma, const size_t scale = static_cast<float>(SIZE))
+    GammaTable(const float gamma, const std::size_t scale = static_cast<float>(SIZE))
     : gamma(gamma), scale(scale)
     {
         create();
@@ -25,8 +25,8 @@ public:
         create();
     }
 
-    const TYPE& operator [](const size_t index) const { return gamma_array[index]; }
-    const size_t size() const { return gamma_array.size(); }
+    const TYPE& operator [](const std::size_t index) const { return gamma_array[index]; }
+    const std::size_t size() const { return gamma_array.size(); }
 
     const float getGamma() const { return gamma; }
     const float getRange() const { return scale; }
@@ -35,15 +35,15 @@ private:
 
     void create()
     {
-        for (size_t i = 0; i < gamma_array.size(); ++i)
+        for (std::size_t i = 0; i < gamma_array.size(); ++i)
         {
-            gamma_array[i] = static_cast<TYPE>(pow(static_cast<float>(i) / static_cast<float>(SIZE), gamma) * scale);
+            gamma_array[i] = static_cast<TYPE>(std::pow(static_cast<float>(i) / static_cast<float>(SIZE), gamma) * scale);
         }
     }
 
     float gamma;
     float scale;
-    RingQueue<TYPE> gamma_array {size};
+    RingQueue<TYPE, SIZE> gamma_array;
 };
 
 #endif // EMBEDDEDUTILS_GAMMA_H
